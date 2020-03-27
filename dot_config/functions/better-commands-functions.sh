d742cd0b4f51b2c73fdcab2e1e1c094f6cb25929
qaraluch <prezenciq@gmail.com>
2020-03-27 10:24:53 +0100

    Add common shell aliases
    Usprawnienie popularnych komend:
    - cp
    - ln
    - ls
    - grep
    - mv
    - rm
    - tree
    
    Nawigacja pomiędzy katalogami:
    - cd
    - dirs
    
    Nowe ulepszone komendy (better-commands):
    - show-dirs as d alias
    - ag-here as gh alias
    - mkdir-and-cd as take alias
    - tree-better and tree-better-dirs as tre i tred aliases
    - vf
    
    Inne:
    - exit as e alias
    
    Przeniesiono wszystkie ustawienia zsh do osobnego pliku.
    
    Changes to be committed:
            modified:   dot_config/aliases
            new file:   dot_config/functions/better-commands-functions.sh
            new file:   dot_config/zsh/dot_zshrc-options
            modified:   dot_config/zsh/dot_zshrc.tmpl

---
+++ dot_config/functions/better-commands-functions.sh
@@ -0,0 +1,29 @@
+# Improve commands
+# see aliases: .config/aliases
+
+# take alias
+# from oh-my-zsh
+mkdir-and-cd() {
+    mkdir -p $@ && cd ${@:$#}
+}
+
+# gh alias (from grep here)
+ag-here() {
+  local flags=(--follow --hidden --ignore .git --ignore .cache --ignore node_modules --ignore package-lock.json)
+  ag "${flags[@]}" $1 ./
+}
+
+# tre alias
+# is a shorthand for `tree` with hidden files and color enabled, ignoring
+# the certain directories, listing directories first.
+# The output gets piped into `less` with options to preserve color and line numbers,
+# unless the output is small enough for one screen.
+tree-better() {
+  tree -aC -I '.git|.cache|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+}
+
+tree-better-dirs() {
+  tree -dC -a -I '.git|.cache|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+    # -a show hidden dirs
+}
+
