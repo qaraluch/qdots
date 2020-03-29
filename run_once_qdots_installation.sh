62b7b14788218217fcbef0db007b6c5291d2ef7c
qaraluch <prezenciq@gmail.com>
2020-03-29 13:01:10 +0200

    Add qdots installation scripts (chezmoi)
    Do instalacji programów niezbędnych do działania qdots.
    
    Lista programów:
    - fzf
    - the_silver_searcher
    - shellcheck
    - moreutils
    
    Changes to be committed:
    	new file:   run_once_qdots_installation.sh

---
+++ run_once_qdots_installation.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# qdots - programs needed for this dotfiles
+
+readonly packagesToInstall=(
+  'fzf'
+  'the_silver_searcher'
+  'shellcheck'
+  'moreutils'
+)
+
+# utils
+readonly _QDel='[ qdots ]'
+readonly _Qcg=$'\033[1;32m'              # Green
+readonly _Qcy=$'\033[1;33m'              # Yellow
+readonly _Qce=$'\033[0m'                 # End
+readonly _Qiw="[ ${_Qcy}!${_Qce} ]"      # Warn
+readonly _Qit="[ ${_Qcg}✔${_Qce} ]"      # Tick
+__echoIt() {
+  local delimiter=$1 ; local msg=$2 ; local icon=${3:-''} ; echo "${delimiter}${icon} $msg" >&2
+}
+__msgNotInstalled() {
+  __echoIt "${_QDel}" "It seems that package: ${_Qcy}$1${_Qce} not installed! Intalling... " "${_Qiw}"
+}
+__msgInstalled() {
+  __echoIt "${_QDel}" "Package: ${_Qcy}$1${_Qce} already installed! Nothing to do." "${_Qiw}"
+}
+__isInstalledOnArch() {
+    # duplicate with fn in general-functions.sh file
+    # have to be a standalone script - can not depend on other helpers
+    local package
+    package="$1"
+    local check
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")"
+    if [ -n "${check}" ] ; then
+        return 0 #installed
+    fi;
+    return 1 # not installed
+}
+
+installPackage() {
+  local package
+  package=${1}
+  local isPackageInstalled
+  isPackageInstalled=$(__isInstalledOnArch "${package}")
+  if ${isPackageInstalled} ; then
+    __msgInstalled "${package}"
+  else
+    __msgNotInstalled "${package}"
+    sudo pacman -S --noconfirm "${package}"
+  fi
+}
+
+for pkg in "${packagesToInstall[@]}" ; do
+  installPackage $pkg
+done
+
